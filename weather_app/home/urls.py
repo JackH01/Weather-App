c00f2f9cfb9153802159bb89f985dff925168880
JackH01 <jackhicks01@outlook.com>
2025-02-18 16:29:09 +0000

    Added redirect from root to 'home\'

--- weather_app/home/urls.py
+++ weather_app/home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.shortcuts import redirect
 from . import views
 from .forms import CityForm
 
 urlpatterns = [
+    path('', lambda req: redirect('home/')),
     path('home/', views.home, name='home'),
 ]
